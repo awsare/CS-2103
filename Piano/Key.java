4225e86f7198b468e38d1182a27f182d08258868
Neel Awsare <neelawsare@gmail.com>
2024-10-23 14:56:55 -0400

    Initial commit

---
+++ Piano/Key.java
@@ -0,0 +1,113 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.sound.midi.*;
+import javax.swing.*;
+
+/**
+ * Implements a key on a simulated piano keyboard.
+ */
+public class Key {
+	// You are free to add more instance variables if you wish.
+	private Polygon _polygon;
+	private int _pitch;
+	private int _keyPosition;
+	private boolean _isOn;
+	private Piano _piano;
+	private boolean _isBlack;
+
+	/**
+	 * Returns the polygon associated with this key.
+	 * @return the polygon associated with this key.
+	 */
+	public Polygon getPolygon () {
+		return _polygon;
+	}
+
+	// You are free to modify the constructor if you wish.
+	/**
+	 * @param polygon the Polygon that describes the shape and position of this key.
+	 * @param pitch the pitch value of the key.
+	 * @param piano the Piano associated with this key.
+	 */
+	public Key (Polygon polygon, int pitch, Piano piano) {
+		_polygon = polygon;
+		_pitch = pitch;
+		_piano = piano;
+		_keyPosition = (this._pitch - 48) % 12;
+		_isBlack = (_keyPosition < 5 && _keyPosition % 2 == 1) || (_keyPosition >= 5 && _keyPosition % 2 == 1);
+	}
+
+	// DO NOT MODIFY THIS METHOD.
+	/**
+	 * Turns the note either on or off.
+	 * @param isOn whether the note should be turned on.
+	 */
+	public void play (boolean isOn) {
+		try {
+
+			// Some MIDI technicalities; don't worry too much about it.
+			ShortMessage myMsg = new ShortMessage();
+			final int VELOCITY = 93;
+			myMsg.setMessage(isOn ? ShortMessage.NOTE_ON : ShortMessage.NOTE_OFF, 0, _pitch, VELOCITY);
+			
+			final int IMMEDIATELY = -1;
+			
+			// Send the message to the receiver (either local or remote).
+			_piano.getReceiver().send(myMsg, IMMEDIATELY);
+			
+			// Set the key to "on".
+			_isOn = isOn;
+			
+			// Ask the piano to redraw itself (since one of its keys has changed).
+			_piano.repaint();
+
+		} catch (InvalidMidiDataException imde) {
+			System.out.println("Could not play key!");
+		}
+	}
+
+	// TODO implement this method.
+	/**
+	 * Paints the key using the specified Swing Graphics object.
+	 * @param g the Graphics object to be used for painting.
+	 */
+	public void paint (Graphics g) {
+		// TODO: Change this to handle the different key colors
+		// and different key states (pressed down or not).
+		int keyPosition = (this._pitch - 48) % 12;
+
+
+		if (this._isOn) {
+			g.setColor(Color.GRAY);
+		} else if (_isBlack) {
+			g.setColor(Color.BLACK);
+		} else {
+			g.setColor(Color.WHITE);
+		}
+		
+		g.fillPolygon(_polygon);
+		g.setColor(Color.BLACK);
+		g.drawPolygon(_polygon);
+	}
+
+	public boolean isBlack() {
+		return this._isBlack;
+	}
+
+	public boolean isOn() {
+		return this._isOn;
+	}
+
+	public int getPitch() {
+		return this._pitch;
+	}
+
+	/**
+	 * Returns a String representation describing the key.
+	 * @return a String representation describing the key.
+	 */
+	public String toString () {
+		return "Key: " + _pitch;
+	}
+}
---
+++ Piano/Piano.java
@@ -0,0 +1,160 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+import javax.sound.midi.*;
+
+/**
+ * Implements a simulated piano with 36 keys.
+ */
+public class Piano extends JPanel {
+	// DO NOT MODIFY THESE CONSTANTS
+	public static int START_PITCH = 48;
+	public static int WHITE_KEY_WIDTH = 40;
+	public static int BLACK_KEY_WIDTH = WHITE_KEY_WIDTH/2;
+	public static int WHITE_KEY_HEIGHT = 200;
+	public static int BLACK_KEY_HEIGHT = WHITE_KEY_HEIGHT/2;
+	public static int NUM_WHITE_KEYS_PER_OCTAVE = 7;
+	public static int NUM_BLACK_KEYS_PER_OCTAVE = 5;
+	public static int NUM_OCTAVES = 3;
+	public static int NUM_WHITE_KEYS = NUM_WHITE_KEYS_PER_OCTAVE * NUM_OCTAVES;
+	public static int KEYS_PER_OCTAVE = NUM_BLACK_KEYS_PER_OCTAVE + NUM_WHITE_KEYS_PER_OCTAVE;
+	public static int WIDTH = NUM_WHITE_KEYS * WHITE_KEY_WIDTH;
+	public static int HEIGHT = WHITE_KEY_HEIGHT;
+		
+	private java.util.List<Key> _keys = new ArrayList<>();
+	private Receiver _receiver;
+	private PianoMouseListener _mouseListener;
+
+	/**
+	 * Returns the list of keys in the piano.
+	 * @return the list of keys.
+	 */
+	public java.util.List<Key> getKeys () {
+		return _keys;
+	}
+
+	/**
+	 * Sets the MIDI receiver of the piano to the specified value.
+	 * @param receiver the MIDI receiver 
+	 */
+	public void setReceiver (Receiver receiver) {
+		_receiver = receiver;
+	}
+
+	/**
+	 * Returns the current MIDI receiver of the piano.
+	 * @return the current MIDI receiver 
+	 */
+	public Receiver getReceiver () {
+		return _receiver;
+	}
+
+	// DO NOT MODIFY THIS METHOD.
+	/**
+	 * @param receiver the MIDI receiver to use in the piano.
+	 */
+	public Piano (Receiver receiver) {
+		// Some Swing setup stuff; don't worry too much about it.
+		setFocusable(true);
+		setLayout(null);
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+		setReceiver(receiver);
+		_mouseListener = new PianoMouseListener(_keys);
+		addMouseListener(_mouseListener);
+		addMouseMotionListener(_mouseListener);
+		makeKeys();
+	}
+
+	/**
+	 * Returns the PianoMouseListener associated with the piano.
+	 * @return the PianoMouseListener associated with the piano.
+	 */
+	public PianoMouseListener getMouseListener () {
+		return _mouseListener;
+	}
+
+	// TODO: implement this method. You should create and use several helper methods to do so.
+	/**
+	 * Instantiate all the Key objects with their correct polygons and pitches, and
+	 * add them to the _keys array.
+	 */
+	private void makeKeys () {
+		for (int i = 0; i < NUM_OCTAVES; i++) {
+			makeOctave(i * WHITE_KEY_WIDTH * NUM_WHITE_KEYS_PER_OCTAVE, START_PITCH + (i * KEYS_PER_OCTAVE));
+		}
+	}
+
+
+	private void makeOctave(int octaveXStart, int octavePitchStart) {
+		int currentWhitePitch = octavePitchStart;
+		int currentBlackPitch = octavePitchStart + 1;
+
+		for (int i = 0; i < NUM_WHITE_KEYS_PER_OCTAVE; i++) {
+			makeWhiteKey(octaveXStart + (i * WHITE_KEY_WIDTH), currentWhitePitch);
+			currentWhitePitch = currentWhitePitch + 2;
+		}
+
+		for (int i = 0; i < NUM_WHITE_KEYS_PER_OCTAVE; i++) {
+			if (i != 2 && i != 6) {
+				makeBlackKey(octaveXStart + (i * WHITE_KEY_WIDTH), currentBlackPitch);
+			}
+			currentBlackPitch = currentBlackPitch + 2;
+		}
+	}
+
+	private void makeBlackKey(int keyXstart, int pitch) {
+		int[] xCoords = new int[] {
+			keyXstart + WHITE_KEY_WIDTH - (BLACK_KEY_WIDTH / 2),
+			keyXstart + WHITE_KEY_WIDTH + (BLACK_KEY_WIDTH / 2),
+			keyXstart + WHITE_KEY_WIDTH + (BLACK_KEY_WIDTH / 2),
+			keyXstart + WHITE_KEY_WIDTH - (BLACK_KEY_WIDTH / 2)
+		};
+
+		int[] yCoords = new int[] {
+			0,
+			0,
+			BLACK_KEY_HEIGHT,
+			BLACK_KEY_HEIGHT
+		};
+		Polygon polygon = new Polygon(xCoords, yCoords, xCoords.length);
+		Key key = new Key(polygon, pitch, this);
+
+		_keys.add(key);
+	}
+
+	private void makeWhiteKey(int keyXstart, int pitch) {
+		int[] xCoords = new int[] {
+			keyXstart,
+			keyXstart + WHITE_KEY_WIDTH,
+			keyXstart + WHITE_KEY_WIDTH,
+			keyXstart
+		};
+
+		int[] yCoords = new int[] {
+			0,
+			0,
+			WHITE_KEY_HEIGHT,
+			WHITE_KEY_HEIGHT
+		};
+		Polygon polygon = new Polygon(xCoords, yCoords, xCoords.length);
+		Key key = new Key(polygon, pitch, this);
+
+		_keys.add(key);
+	}
+
+
+	// DO NOT MODIFY THIS METHOD.
+	@Override
+	/**
+	 * Paints the piano and all its constituent keys.
+	 * @param g the Graphics object to use for painting.
+	 */
+	public void paint (Graphics g) {
+		// Delegates to all the individual keys to draw themselves.
+		for (Key key: _keys) {
+			key.paint(g);
+		}
+	}
+}
