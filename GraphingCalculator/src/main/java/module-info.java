3d8f74e8a768433c7ea2f651eea6bc4268d60d95
Neel Awsare <neelawsare@gmail.com>
2024-12-07 16:01:26 -0500

    removed graphing calc

--- GraphingCalculator/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.graphingcalculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.graphingcalculator to javafx.fxml;
-    exports org.example.graphingcalculator;
-}
